90c3ba8682256eb50899837e45058ddd7a87b438
yasin <yassingourkani@outlook.com>
2023-05-04 21:22:09 +0330

    add evaluation for TSP problem.

---
+++ services/tsp/evalTSP.c
@@ -0,0 +1,26 @@
+#include "../../headers/mainHeaders.h"
+
+// make array of evaluation of DIS matrix
+int *evalTSP(const int *matrix, int size) {
+    int row, col, sum, *tempArr = (int *) calloc(size, sizeof(int));
+
+    for (int i = 0; i < size; i++) {
+        sum = 0;
+
+        for (int j = 0; j < size - 1; j++) {
+            // get row of each sol
+            row = matrix[i * size + j];
+
+            // get col of each sol
+            col = matrix[i * size + j + 1];
+
+            sum += matrix[row * size + col];
+        }
+
+        sum += matrix[(size - 1) * size];
+
+        tempArr[i] = sum;
+    }
+
+    return tempArr;
+}
--- services/tsp/tspMain.c
+++ services/tsp/tspMain.c
@@ -1,11 +1,16 @@
 #include "../../services/tsp/initTSP.c"
+#include "./evalTSP.c"
 
 void tspMain() {
-    int citiesSize, *disMatrix;
+    int citiesSize, *disMatrix, *evalDisMatrix;
 
     citiesSize = getTSPInitValues();
 
     disMatrix = setDisSol(citiesSize);
 
+    evalDisMatrix = evalTSP(disMatrix, citiesSize);
+
     printMatrix(citiesSize, disMatrix);
+
+    printArray(citiesSize, evalDisMatrix, "Evaluation: ");
 }
