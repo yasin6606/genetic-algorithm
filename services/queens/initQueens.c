be529cbf9b6c6d4dac09ae7e086dd3eac8025234
yasin <yassingourkani@outlook.com>
2023-05-06 15:55:37 +0330

    make queensSol generator faster by reducing its loops.

--- services/queens/initQueens.c
+++ services/queens/initQueens.c
@@ -17,18 +17,34 @@ int getQueensInitValues() {
 }
 
 int *setQueensSol(int nQueens) {
-    int *queensArr = (int *) calloc(nQueens * nQueens, sizeof(int));
-    int *row;
+    int *queensArr = (int *) calloc(nQueens * nQueens, sizeof(int)), i = 0, j = 0;
 
-    for (int i = 0; i < nQueens; i++) {
-        row = makePerm(nQueens);
-
-        for (int j = 0; j < nQueens; j++) {
-            queensArr[i * nQueens + j] = row[j];
+    while (i < nQueens) {
+        if (j == nQueens) {
+            i++;
+            j = 0;
         }
-    }
 
-    printMatrix(nQueens, queensArr);
+        // add each element to each sol
+        queensArr[i * nQueens + j] = makePerm(nQueens)[j];
+
+        j++;
+    }
 
     return queensArr;
 }
+
+//// O(n^2) Old method
+//int *setQueensSol(int nQueens) {
+//    int *queensArr = (int *) calloc(nQueens * nQueens, sizeof(int));
+//    int *row = NULL;
+//
+//    for (int i = 0; i < nQueens; i++) {
+//        row = makePerm(nQueens);
+//
+//        for (int j = 0; j < nQueens; j++)
+//            queensArr[i * nQueens + j] = row[j];
+//    }
+//
+//    return queensArr;
+//}
