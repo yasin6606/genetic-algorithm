cbe70008526f0fd01f21e7cbfd2b57a5031cbdc8
yasin <yassingourkani@outlook.com>
2023-05-06 15:53:38 +0330

    add evaluation for queens problem.

---
+++ services/queens/evalQueens.c
@@ -0,0 +1,49 @@
+#include "../../headers/mainHeaders.h"
+
+int *evalQueens(const int *matrix, int size);
+
+int evalSolQueen(const int *sol, int size);
+
+/*
+ * To find diagonal collisions, just find differences between each queen's location (row, col) with other queens; If
+ * differences are the same so these queens are met each other.
+ * */
+int *evalQueens(const int *matrix, int size) {
+    int *tempArr = (int *) calloc(size, sizeof(int));
+
+    for (int i = 0; i < size; i++)
+        tempArr[i] = evalSolQueen(&matrix[i * size], size);
+
+    return tempArr;
+}
+
+//// O(n) new method
+int evalSolQueen(const int *sol, int size) {
+    int collision = 0, i = 0, j = i + 1;
+
+    while (i < size - 2) {
+        if (j >= size) {
+            i++;
+            j = i + 1;
+        }
+
+        if (abs(sol[i] - sol[j]) == abs(i - j))
+            collision++;
+
+        j++;
+    }
+
+    return collision;
+}
+
+//// O(n^2) old method
+//int evalSolQueen(const int *sol, int size) {
+//    int collision = 0;
+//
+//    for (int i = 0; i < size - 1; i++)
+//        for (int j = i; j < size - 1; j++)
+//            if (abs(sol[i] - sol[j + 1]) == abs(i - (j + 1)))
+//                collision++;
+//
+//    return collision;
+//}
--- services/queens/queensMain.c
+++ services/queens/queensMain.c
@@ -1,9 +1,22 @@
 #include "./initQueens.c"
+#include "./evalQueens.c"
 
 void queensMain() {
     int n, *queensMatrix = NULL, *e = NULL;
 
     n = getQueensInitValues();
 
-    setQueensSol(n);
+    if (n < 4) {
+        printf("\nMinimum queens must be 4 or upper\n");
+        return;
+    }
+
+    // make initial population
+    queensMatrix = setQueensSol(n);
+
+    // evaluating made population
+    e = evalQueens(queensMatrix, n);
+
+    printMatrix(n, queensMatrix);
+    printArray(n, e, "Evaluation (Collisions are counted): ");
 }
