c080be2202fa27af13c52c6dfe1cd37726687080
yasin <yassingourkani@outlook.com>
2023-06-04 15:19:05 +0330

    add a directory for menus and do need chang (separate menus).

---
+++ headers/menu.h
@@ -0,0 +1,18 @@
+//
+// Created by yasin on 6/4/23.
+//
+
+#ifndef GENETIC_ALGORITHM_MENU_H
+#define GENETIC_ALGORITHM_MENU_H
+
+#include "../menus/showServices.c"
+#include "../menus/getService.c"
+#include "../menus/setService.c"
+
+void showServices();
+
+int getService();
+
+void setService(size_t serviceNum);
+
+#endif //GENETIC_ALGORITHM_MENU_H
