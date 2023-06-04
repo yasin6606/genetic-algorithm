527f26b1404bf67a637f15f136ebe32548952613
yasin <yassingourkani@outlook.com>
2023-06-04 22:18:47 +0330

    change some colors and add a text.

--- menus/mainMenus/showServices.c
+++ menus/mainMenus/showServices.c
@@ -5,16 +5,26 @@ void showServices() {
     char servicesName[SERVICES_LEN][10] = {"TSP", "Knapsack", "N-Queens", "Exit"};
 
     printf("Hello\t%s\n\n", __DATE__);
+
     printf(
-            "This program is created by %sYasin Gourkani%s\tStudent ID: %s9974359%s\n\n%sShiraz University%s\n\n",
+            "This program is created by %sYasin Gourkani%s\tStudent ID: %s9974359%s\n\nUniversity: %sShiraz University%s\n\n",
             ANSI_COLOR_BLUE,
             ANSI_COLOR_RESET,
             ANSI_COLOR_BLUE,
             ANSI_COLOR_RESET,
-            ANSI_COLOR_RED,
+            ANSI_COLOR_YELLOW,
             ANSI_COLOR_RESET
     );
+
     printf("E-Mail: %syassingourkani@outlook.com%s\n\n", ANSI_COLOR_YELLOW, ANSI_COLOR_RESET);
+
+    printf(
+            "%s(ATTENTION)%s This program is running on Multi-Process mode!%s\n\n",
+            ANSI_COLOR_RED,
+            ANSI_COLOR_BLUE,
+            ANSI_COLOR_RESET
+    );
+
     printf("Application Menu: \n\n");
 
     for (int i = 0; i < SERVICES_LEN; i++)
