cf87442a3090a7d990d25d84dcf3b465ec4222e3
yasin <yassingourkani@outlook.com>
2023-06-06 16:02:19 +0330

    add some macros.

--- headers/sharedMacros.h
+++ headers/sharedMacros.h
@@ -10,6 +10,8 @@
 #define PARENTS_NUM 2
 #define CROSSOVER_TYPE_LEN 2
 
+#define N_TIMES(X, Y) (X * Y)
+
 // String colors
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_GREEN   "\x1b[32m"
@@ -19,12 +21,16 @@
 #define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
-//#define FONT_COL_CUSTOM (R,G,B)   ("\e[38;2;" " " R " " ";" + G + ";" + B + "m")
-#define BCKGRD_COL_CUSTOM_RED   "\e[48;2;200;0;0m"
-#define BCKGRD_COL_CUSTOM_GREEN "\e[48;2;0;200;0m"
-#define BCKGRD_COL_CUSTOM_BLUE  "\e[48;2;0;0;200m"
+#define FONT_COL_CUSTOM(R, G, B)   "\e[38;2;" #R ";" #G ";" #B "m"
+#define BG_COL_CUSTOM(R, G, B)   "\e[48;2;" #G ";" #G ";" #B "m"
 
 #define GO_FIRST_OF_CURRENT_LINE "\r"
 #define RM_CURRENT_LINE "\33[2K\r"
+
+// Lines
+#define DASHED_LINE puts("\n---------------------------------------------------------------------")
+
+// Debugger
+#define SHOW_NUM(X) printf("\n%d \n", X);
 
 #endif //GENETIC_ALGORITHM_SHAREDMACROS_H
