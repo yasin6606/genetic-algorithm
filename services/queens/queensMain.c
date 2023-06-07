c9cd8bd335c2047409d91c4855e2134eb2f34037
yasin <yassingourkani@outlook.com>
2023-06-07 11:54:48 +0330

    add crossover to n-queens problem.

--- services/queens/queensMain.c
+++ services/queens/queensMain.c
@@ -3,12 +3,14 @@
 #include "../../headers/printing.h"
 #include "../../headers/sharedMenu.h"
 #include "../../headers/multiproseccing/multiprocessor.h"
+#include "../../headers/crossover.h"
 
 void queensMain() {
-    int queensNum, *queensMatrix = NULL, *evalResult = NULL;
+    int queensNum, crossoverType, *queensMatrix = NULL, *evalResult = NULL, *bestParentsIdx = NULL, *firstChild = NULL, *secondChild = NULL;
 
     // Get the number of queens
     queensNum = intInput("Enter the number of Queens: ");
+    crossoverType = crossoverMenu();
 
     if (queensNum < 4) {
         printf("\nMinimum queens must be 4 or upper\n");
@@ -20,6 +22,36 @@ void queensMain() {
 
     // evaluating made population
     evalResult = multiprocessor(queensNum, queensNum, &evalQueens, 1, queensMatrix);
+
+    // Selection parents based on K-Competition algorithm
+    bestParentsIdx = parentSelection(evalResult, queensNum, true);
+
+    firstChild = (int *) calloc(queensNum, sizeof(int));
+    secondChild = (int *) calloc(queensNum, sizeof(int));
+
+    // Crossover
+    if (crossoverType == 1) { // Two breaking points
+        crossover2P(
+                &queensMatrix[bestParentsIdx[0] * queensNum],
+                &queensMatrix[bestParentsIdx[1] * queensNum],
+                queensNum,
+                false,
+                firstChild,
+                secondChild
+        );
+    } else { // Uniform
+        crossoverUni(
+                &queensMatrix[bestParentsIdx[0] * queensNum],
+                &queensMatrix[bestParentsIdx[1] * queensNum],
+                queensNum,
+                false,
+                firstChild,
+                secondChild
+        );
+    }
+
+    printArray(queensNum, firstChild, "First Child: ", ANSI_COLOR_RESET);
+    printArray(queensNum, secondChild, "Second Child: ", ANSI_COLOR_RESET);
 
     printMatrix(queensNum, queensMatrix, true);
 
