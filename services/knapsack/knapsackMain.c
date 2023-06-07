419605c9d61ff4bca5437230a78d216d4fdd85e0
yasin <yassingourkani@outlook.com>
2023-06-07 11:42:19 +0330

    add crossover to knapsack problem.

--- services/knapsack/knapsackMain.c
+++ services/knapsack/knapsackMain.c
@@ -3,12 +3,16 @@
 #include "../../headers/printing.h"
 #include "../../headers/multiproseccing/multiprocessor.h"
 #include "../../headers/sharedMenu.h"
+#include "../../headers/crossover.h"
 
 void knapsackMain() {
-    int productsNum, wMax, *wArr, *vArr, *population = NULL, *evalResults = NULL;
+    int productsNum, wMax, crossoverType, *wArr, *vArr, *population = NULL, *evalResults = NULL, *bestParentsIdx,
+            *firstChild, *secondChild;
 
+    // Get initial values
     productsNum = intInput("Enter the number of products: ");
     wMax = intInput("Enter the maximum weight of knapsack: ");
+    crossoverType = crossoverMenu();
 
     // Fill wArr (Weight Array) randomly
     wArr = chromosomeMaker(productsNum, false, false, productsNum, 1, 0);
@@ -31,8 +35,38 @@ void knapsackMain() {
             population
     );
 
-    printArray(productsNum, wArr, "Weight: ", ANSI_COLOR_RESET);
-    printArray(productsNum, vArr, "Value: ", ANSI_COLOR_RESET);
+    // Selection parents based on K-Competition algorithm
+    bestParentsIdx = parentSelection(evalResults, productsNum, true);
+
+    firstChild = (int *) calloc(productsNum, sizeof(int));
+    secondChild = (int *) calloc(productsNum, sizeof(int));
+
+    // Crossover
+    if (crossoverType == 1) { // Two breaking points
+        crossover2P(
+                &population[bestParentsIdx[0] * productsNum],
+                &population[bestParentsIdx[1] * productsNum],
+                productsNum,
+                true,
+                firstChild,
+                secondChild
+        );
+    } else { // Uniform
+        crossoverUni(
+                &population[bestParentsIdx[0] * productsNum],
+                &population[bestParentsIdx[1] * productsNum],
+                productsNum,
+                true,
+                firstChild,
+                secondChild
+        );
+    }
+
+    printArray(productsNum, wArr, "Weight: ", ANSI_COLOR_MAGENTA);
+    printArray(productsNum, vArr, "Value: ", ANSI_COLOR_MAGENTA);
+
+    printArray(productsNum, firstChild, "First Child: ", ANSI_COLOR_RESET);
+    printArray(productsNum, secondChild, "Second Child: ", ANSI_COLOR_RESET);
 
     printMatrix(productsNum, population, true);
 
