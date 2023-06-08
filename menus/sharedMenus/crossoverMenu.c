318be29bcf38b3c86fa573ec12a95f2d6b4070d6
yasin <58929848+yasin6606@users.noreply.github.com>
2023-06-08 11:32:26 +0330

    add fflush to show those pritf functions without ending \n.

--- menus/sharedMenus/crossoverMenu.c
+++ menus/sharedMenus/crossoverMenu.c
@@ -6,11 +6,13 @@ size_t crossoverMenu() {
     char *items[CROSSOVER_TYPE_LEN] = {"Two breaking points", "Uniform"};
 
     printf("\nCrossover menu:\n");
+    fflush(stdout);
 
     for (int i = 0; i < CROSSOVER_TYPE_LEN; i++)
         printf("\t%s%d%s- %s\n", ANSI_COLOR_BLUE, i + 1, ANSI_COLOR_RESET, items[i]);
 
     printf("\nEnter crossover type: ");
+    fflush(stdout);
     scanf("%zu", &temp);
     puts("");
 
