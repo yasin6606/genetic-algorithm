f0df96c85a54bbdef42871e1aadaa749aa2471f7
yasin <yassingourkani@outlook.com>
2023-06-12 11:31:31 +0330

    change file name. IMPORTANT

--- headers/crossover.h
+++ headers/crossover.h
@@ -5,7 +5,7 @@
 #ifndef GENETIC_ALGORITHM_CROSSOVER_H
 #define GENETIC_ALGORITHM_CROSSOVER_H
 
-#include "../functions/crossover.c"
+#include "../functions/crossoverTypes.c"
 
 void crossover2P(int *firstParent, int *secondParent, size_t populationNum, bool ignorePerm, int *firstChild,
                  int *secondChild);
