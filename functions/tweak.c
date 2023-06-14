9ad6c47cfd5690cb027ee291b549a1ae6ec6a78e
yasin <yassingourkani@outlook.com>
2023-06-14 22:21:27 +0330

    develop tweak function so as to mutation on multi chromosomes.

--- functions/tweak.c
+++ functions/tweak.c
@@ -1,8 +1,14 @@
 #include "../headers/assets.h"
 #include "../headers/makers.h"
 
-void tweak(int *sol, size_t populationNum) {
-    int *rands = chromosomeMaker(populationNum, false, true, 2, 0);
+void tweak(int *population, size_t populationNum) {
+    int mutationPercent, *rands = NULL;
 
-    swap(sol, rands[0], rands[1]);
+    mutationPercent = ceil(populationNum * MUTATION_PERCENT);
+
+    for (int i = 0; i < mutationPercent; i++) {
+        rands = chromosomeMaker(populationNum, false, true, 2, 0);
+
+        swap(&population[i * populationNum], rands[0], rands[1]);
+    }
 }
