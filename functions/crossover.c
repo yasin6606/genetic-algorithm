0c862b74a3150dc1452b5868a4a2080b9666f676
yasin <yassingourkani@outlook.com>
2023-06-14 19:44:36 +0330

    make crossover algorithm better than the past. Also merge crossover and crossoverTypes. Moreover, make crossover multiprocess. IMPORTANT

---
+++ functions/crossover.c
@@ -0,0 +1,89 @@
+#include "../headers/sharedLib.h"
+#include "../headers/makers.h"
+
+/*
+ * Two breaking points VS Uniform:
+ * crossover2P generates two new different chromosomes based on two breaking points which the first point always should be smaller
+ * than the second one and the difference between points must be greater than 1 so as to avoid getting two same points.
+ * Moreover, this function give an argument that is called ignorePerm which define new chromosomes should be permutation or not.
+ * Also, There is a mask array which is a binary array to generate a new child based on Uniform algorithm; By using this type of
+ * generating algorithm, it will select gens which are masked on 1 from one parent and 0 on another parent. As well as, there is a
+ * checking code so as to produce a permutation child based on ignorePerm's value.
+ * */
+
+void crossover(size_t populationNum, size_t childShare, int *sharedMem, int startIdx, size_t argsNum, va_list args) {
+    int row, *bestParentsIdx = NULL, *breakPoints = NULL, *mask = NULL, *newChild = NULL, *population = NULL,
+            *evalResult = NULL, *evalSortedIdx = NULL, ignorePerm, crossoverType, elitesNum;
+
+    // Last population
+    population = va_arg(args,
+    int *);
+
+    // Last evaluation result
+    evalResult = va_arg(args,
+    int* );
+
+    // Sorted last evaluation result in order to move some of them (4%) which must directly add to new population.
+    evalSortedIdx = va_arg(args,
+    int* );
+
+    // Controller to ignore permutation or not
+    ignorePerm = va_arg(args,
+    int);
+
+    // Controller so as to generate new children based on crossover type
+    crossoverType = va_arg(args,
+    int);
+
+    // The number of elite chromosomes which must directly move to new population
+    elitesNum = va_arg(args,
+    int);
+
+    for (int i = 0; i < childShare; i++) {
+        row = startIdx + (i * populationNum);
+
+        // Add those chromosomes which must directly move to new population.
+        if ((row / populationNum) < elitesNum) {
+            for (int j = 0; j < populationNum; j++)
+                sharedMem[row + j] = population[(evalSortedIdx[i] * populationNum) + j];
+
+            continue;
+        }
+
+        // Selection parents based on K-Competition algorithm
+        bestParentsIdx = parentSelection(evalResult, populationNum, true);
+
+        // Crossover
+        if (crossoverType == 1) { // Two breaking points
+
+            // Get break points
+            breakPoints = breakPointGenerator(populationNum);
+
+            // Generate a new child (chromosome)
+            newChild = childGenerator2P(
+                    populationNum,
+                    ignorePerm,
+                    breakPoints,
+                    &population[bestParentsIdx[0] * populationNum],
+                    &population[bestParentsIdx[1] * populationNum]
+            );
+        } else { // Uniform
+
+            // Generate a mask array so as to produce a new child
+            mask = chromosomeMaker(populationNum, true, false, -1, 0);
+
+            // Generate a new child (chromosome)
+            newChild = childGeneratorUni(
+                    populationNum,
+                    ignorePerm,
+                    mask,
+                    &population[bestParentsIdx[0] * populationNum],
+                    &population[bestParentsIdx[1] * populationNum]
+            );
+        }
+
+        // Add a new generated child to new population
+        for (int j = 0; j < populationNum; j++)
+            sharedMem[row + j] = newChild[j];
+    }
+}
--- headers/makers.h
+++ headers/makers.h
@@ -10,6 +10,7 @@
 #include "../functions/breakPointGenerator.c"
 #include "../functions/childGenerator.c"
 #include "../functions/parentSelection.c"
+#include "../functions/crossover.c"
 
 int *chromosomeMaker(size_t maxGenNum, bool isBin, bool isPerm, int limitLen, size_t ignoreNumsCount, ...);
 
@@ -17,11 +18,12 @@ void tweak(int *sol, size_t size);
 
 int *breakPointGenerator(size_t populationNum);
 
-int *childGenerator2P(size_t populationNum, int *breakPoints, bool ignorePerm, int *oneParent, int *anotherParent);
+int *childGenerator2P(size_t populationNum, bool ignorePerm, int *breakPoints, int *oneParent, int *anotherParent);
 
 int *childGeneratorUni(size_t populationNum, bool ignorePerm, int *mask, int *oneParent, int *anotherParent);
 
 void *parentSelection(void *evaluationArr, size_t populationNum, bool type);
 
+void crossover(size_t populationNum, size_t childShare, int *sharedMem, int startIdx, size_t argsNum, va_list args);
 
 #endif //GENETIC_ALGORITHM_MAKERS_H
