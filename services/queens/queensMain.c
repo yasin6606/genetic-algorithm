6e39f64e0cedeee3abb156322d4a6adca1e9c5b0
yasin <yassingourkani@outlook.com>
2023-06-14 22:24:14 +0330

    add crossover with multiprocess mode on queens' problem. IMPORTANT

--- services/queens/queensMain.c
+++ services/queens/queensMain.c
@@ -3,14 +3,17 @@
 #include "../../headers/printing.h"
 #include "../../headers/sharedMenu.h"
 #include "../../headers/multiproseccing/multiprocessor.h"
-#include "../../headers/crossover.h"
+#include "../../headers/sharedMacros.h"
 
 void queensMain() {
-    int queensNum, crossoverType, *queensMatrix = NULL, *evalResult = NULL, *bestParentsIdx = NULL, *firstChild = NULL, *secondChild = NULL;
+    int queensNum, crossoverType, elitePercent, *queensMatrix = NULL, *evalResult = NULL, *evalSortedIdx = NULL;
 
     // Get the number of queens
     queensNum = intInput("Enter the number of Queens: ");
     crossoverType = crossoverMenu();
+
+    // Get the number of chromosomes which must move to new population directly
+    elitePercent = ceil(queensNum * ELITE_PERCENT);
 
     if (queensNum < 4) {
         printf("\nMinimum queens must be 4 or upper\n");
@@ -23,32 +26,22 @@ void queensMain() {
     // evaluating made population
     evalResult = multiprocessor(queensNum, queensNum, &evalQueens, 1, queensMatrix);
 
-    // Selection parents based on K-Competition algorithm
-    bestParentsIdx = parentSelection(evalResult, queensNum, true);
-
-    firstChild = (int *) calloc(queensNum, sizeof(int));
-    secondChild = (int *) calloc(queensNum, sizeof(int));
+    // Sort evaluation result in order to direct send 4% of the best chromosomes
+    evalSortedIdx = sortChromosomes(evalResult, queensNum);
 
     // Crossover
-    if (crossoverType == 1) { // Two breaking points
-        crossover2P(
-                &queensMatrix[bestParentsIdx[0] * queensNum],
-                &queensMatrix[bestParentsIdx[1] * queensNum],
-                queensNum,
-                false,
-                firstChild,
-                secondChild
-        );
-    } else { // Uniform
-        crossoverUni(
-                &queensMatrix[bestParentsIdx[0] * queensNum],
-                &queensMatrix[bestParentsIdx[1] * queensNum],
-                queensNum,
-                false,
-                firstChild,
-                secondChild
-        );
-    }
+    queensMatrix = multiprocessor(
+            queensNum,
+            queensNum * queensNum,
+            &crossover,
+            6,
+            queensMatrix,
+            evalResult,
+            evalSortedIdx,
+            false,
+            crossoverType,
+            elitePercent
+    );
 
     printArray(queensNum, firstChild, "First Child: ", ANSI_COLOR_RESET);
     printArray(queensNum, secondChild, "Second Child: ", ANSI_COLOR_RESET);
@@ -56,4 +49,5 @@ void queensMain() {
     printMatrix(queensNum, queensMatrix, true);
 
     printArray(queensNum, evalResult, "Evaluation (Collisions are counted): ", ANSI_COLOR_RESET);
+    printArray(queensNum, evalSortedIdx, "Evaluation Sorted Indexes (Collisions are counted): ", ANSI_COLOR_RESET);
 }
