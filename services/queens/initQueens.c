37dc8d564e033a9fbffefff4c68a8155ec97a2a0
yasin <yassingourkani@outlook.com>
2023-06-16 14:58:52 +0330

    initialize a pointer to NULL.

--- services/queens/initQueens.c
+++ services/queens/initQueens.c
@@ -3,7 +3,7 @@
 
 void queensPopulationMaker(size_t populationNum, size_t childShare, int *sharedMem, int startIdx, size_t argsNum,
                            va_list args) {
-    int *tempChromosome;
+    int *tempChromosome = NULL;
 
     for (int i = 0; i < childShare; i++) {
 
