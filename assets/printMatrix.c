e979f521a94587e067c09708737c6b80d2a897c2
yasin <yassingourkani@outlook.com>
2023-06-16 23:24:59 +0330

    refactoring.

--- assets/printMatrix.c
+++ assets/printMatrix.c
@@ -6,10 +6,10 @@ void printMatrix(size_t dimension, int *matrix, bool showRowIdx) {
 
     for (int i = 0; i < dimension; i++) {
         if (showRowIdx)
-            printf("%d %s|%s\t", i, ANSI_COLOR_RED, ANSI_COLOR_RESET);
+            printf("%3d %s|%5s", i, ANSI_COLOR_RED, ANSI_COLOR_RESET);
 
         for (int j = 0; j < dimension; j++)
-            printf("%d\t", matrix[i * dimension + j]);
+            printf("%4d", matrix[i * dimension + j]);
 
         puts("");
     }
