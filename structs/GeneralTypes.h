0ec865126f0f327ab84a3835dab43d985b00b35d
yasin <yassingourkani@outlook.com>
2023-06-18 12:30:44 +0330

    add a typedef struct for sorted evaluations.

---
+++ structs/GeneralTypes.h
@@ -0,0 +1,13 @@
+//
+// Created by yasin on 6/11/23.
+//
+
+#ifndef GENETIC_ALGORITHM_GENERALTYPES_H
+#define GENETIC_ALGORITHM_GENERALTYPES_H
+
+typedef struct {
+    int idx;
+    int value;
+} EvalType;
+
+#endif //GENETIC_ALGORITHM_GENERALTYPES_H
