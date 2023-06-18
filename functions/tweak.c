f327e47e19d6dc71ef5ca3ed461b177f8d99e3e4
yasin <yassingourkani@outlook.com>
2023-06-18 12:25:05 +0330

    get the length of population and make some essential changes on tweak function.

--- functions/tweak.c
+++ functions/tweak.c
@@ -1,14 +1,19 @@
 #include "../headers/assets.h"
 #include "../headers/makers.h"
 
-void tweak(int *population, size_t populationNum) {
-    int mutationPercent, *rands = NULL;
+void tweak(int *population, size_t populationNum, size_t populationLen) {
+    int mutationPercent, *r = NULL, *rands = NULL;
 
-    mutationPercent = ceil(populationNum * MUTATION_PERCENT);
+    mutationPercent = ceil(populationLen * MUTATION_PERCENT);
+
+    r = chromosomeMaker(populationLen, false, true, mutationPercent, 0);
 
     for (int i = 0; i < mutationPercent; i++) {
         rands = chromosomeMaker(populationNum, false, true, 2, 0);
 
-        swap(&population[i * populationNum], rands[0], rands[1]);
+        swap(&population[r[i] * populationNum], rands[0], rands[1]);
     }
+
+    free(r);
+    free(rands);
 }
