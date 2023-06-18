c47ad3bb7b04cf5fef4c04a95b866f0b2e5cb8be
yasin <yassingourkani@outlook.com>
2023-06-18 17:10:30 +0330

    add sorting (Bubble Sort) for chromosomes' evaluation.

---
+++ assets/sortChromosome.c
@@ -0,0 +1,35 @@
+#include "../headers/sharedLib.h"
+#include "../headers/assistant.h"
+#include "../types/GeneralTypes.h"
+
+EvalType *bubbleSort(EvalType *arr, size_t len) {
+    EvalType t;
+
+    for (int i = 0; i < len; i++)
+        for (int j = 0; j < len; j++)
+            if (arr[j].value < arr[j - 1].value) {
+                t = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = t;
+            }
+
+    return arr;
+}
+
+void *sortChromosomes(int *evalArr, size_t len) {
+    EvalType *eval = (EvalType *) calloc(len, sizeof(EvalType));
+    int *evalSorted = (int *) calloc(len, sizeof(int));
+
+    for (int i = 0; i < len; i++) {
+        eval[i].idx = i;
+        eval[i].value = evalArr[i];
+    }
+
+    // Sorting
+    eval = bubbleSort(eval, len);
+
+    for (int i = 0; i < len; i++)
+        evalSorted[i] = eval[i].idx;
+
+    return evalSorted;
+}
--- headers/assistant.h
+++ headers/assistant.h
@@ -9,11 +9,14 @@
 #include "../assets/max.c"
 #include "../assets/min.c"
 #include "../assets/swap.c"
+#include "../assets/sortChromosome.c"
 
 int max(const int *arr, size_t size);
 
 int min(const int *arr, size_t size);
 
 void swap(int *arr, size_t firstIdx, size_t secondIdx);
+
+void *sortChromosomes(int *evalArr, size_t len);
 
 #endif //GENETIC_ALGORITHM_ASSISTANT_H
