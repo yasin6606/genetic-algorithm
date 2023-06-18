7f548bb87fb7ecd0caa21999337e301055b52e9c
yasin <yassingourkani@outlook.com>
2023-06-18 17:13:52 +0330

    add some shared macros.

--- headers/sharedMacros.h
+++ headers/sharedMacros.h
@@ -9,6 +9,7 @@
 #define K_COMPETITION 3
 #define PARENTS_NUM 2
 #define CROSSOVER_TYPE_LEN 2
+#define ELITE_PERCENT 0.04
 #define MUTATION_PERCENT 0.3
 
 // String colors
@@ -20,12 +21,21 @@
 #define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
-//#define FONT_COL_CUSTOM (R,G,B)   ("\e[38;2;" " " R " " ";" + G + ";" + B + "m")
-#define BCKGRD_COL_CUSTOM_RED   "\e[48;2;200;0;0m"
-#define BCKGRD_COL_CUSTOM_GREEN "\e[48;2;0;200;0m"
-#define BCKGRD_COL_CUSTOM_BLUE  "\e[48;2;0;0;200m"
+#define FONT_COL_CUSTOM(R, G, B)   "\e[38;2;" #R ";" #G ";" #B "m"
+#define BG_COL_CUSTOM(R, G, B)   "\e[48;2;" #G ";" #G ";" #B "m"
 
 #define GO_FIRST_OF_CURRENT_LINE "\r"
 #define RM_CURRENT_LINE "\33[2K\r"
+
+// Lines
+#define DASHED_LINE puts("\n---------------------------------------------------------------------");
+
+// Debugger
+#define SHOW_NUM(X) printf("\n%d \n", X);
+#define SLEEP_TESTER(X) printf("Start\n"); sleep(X); printf("End\n");
+
+// Errors and Warnings
+#define SHOW_ERROR(TEXT) printf("\n\n%s(Error) %s %s\n\n", ANSI_COLOR_RED, TEXT, ANSI_COLOR_RESET);
+#define SHOW_WARNING(TEXT) printf("\n\n%s(Wrong) %s %s\n\n", FONT_COL_CUSTOM(255,204,0), TEXT, ANSI_COLOR_RESET);
 
 #endif //GENETIC_ALGORITHM_SHAREDMACROS_H
