745d4cff07ffdd3ccb58ef455e222ba957994278
yasin <yassingourkani@outlook.com>
2023-06-20 10:29:45 +0330

    add color.

--- menus/sharedMenus/crossoverMenu.c
+++ menus/sharedMenus/crossoverMenu.c
@@ -11,9 +11,10 @@ size_t crossoverMenu() {
     for (int i = 0; i < CROSSOVER_TYPE_LEN; i++)
         printf("\t%s%d%s- %s\n", ANSI_COLOR_BLUE, i + 1, ANSI_COLOR_RESET, items[i]);
 
-    printf("\nEnter crossover type: ");
+    printf("\nEnter crossover type: %s", ANSI_COLOR_CYAN);
     fflush(stdout);
     scanf("%zu", &temp);
+    printf("%s", ANSI_COLOR_RESET);
     puts("");
 
     return temp;
