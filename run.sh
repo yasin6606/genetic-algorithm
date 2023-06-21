2d177d93aac4b7076339019f612a5a210f3988ba
yasin <58929848+yasin6606@users.noreply.github.com>
2023-06-21 16:50:51 +0330

    remove a unused file

--- run.sh
+++
@@ -1,8 +0,0 @@
-sudo rm ./runner && ./*.o && rm ./*.png
-gcc -c ./plot/pbPlots.c -std=c99 -O3 -march=native
-gcc -c ./plot/supportLib.c -std=c99 -O3 -march=native
-#gcc -c main.c -lm -std=c99 -O3 -march=native
-gcc main.c pbPlots.o supportLib.o -lm -o runner
-#strip runner
-#gcc main.c -lm -o runner
-./runner
